f85719494b577d08f3f97c16c43cb1206ae0735f
levideutsch <levideutsch@live.com>
2022-12-15 11:01:58 -0500

    commit tree c7deebba5fb3bdeb6fa0a0321303b965f85b6aa2

---
+++ src/context/User.js
@@ -0,0 +1,29 @@
+// src/context/user.js
+import React from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+// create a provider component
+// function UserProvider({ children }) {
+
+//     const [user, setUser] = useState(null);
+//   // the value prop of the provider will be our context data
+//   // this value will be available to child components of this provider
+//   return (
+//    <UserContext.Provider value={{user, setUser}}>
+//     {children}
+//     </UserContext.Provider>;
+//   )
+// }
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+    return (
+      <UserContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserContext.Provider>
+    );
+  }
+
+export { UserContext, UserProvider };
